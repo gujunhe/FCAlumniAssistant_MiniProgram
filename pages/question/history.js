0157d09501d7af81533b1c7b70c7381074a11544
jongkrystal808 <jongkrystal808@gmail.com>
2022-11-25 21:20:46 +0800

    更新答题模块

---
+++ pages/question/history.js
@@ -0,0 +1,75 @@
+/*
+//获取应用实例
+
+const app = getApp()
+
+// 连接云数据库
+const db = wx.cloud.database();
+// 获取集合的引用
+const activityRecord = db.collection('activityRecord');
+// 数据库操作符
+const _ = db.command;
+*/
+Page({
+  data: {
+    historyList: []
+  },
+  
+  onLoad() {
+    this.getHistoryList();
+  },
+
+  goToResult(event){
+    const { id } = event.currentTarget.dataset;
+    wx.navigateTo({
+      url: '../result/result?id=' + id
+    })
+  },
+
+  getHistoryList() {
+    // 显示 loading 提示框
+    wx.showLoading({
+      title: '拼命加载中'
+    });
+    // 数据库集合的聚合操作实例
+    activityRecord
+    .where({       //类似于where，对记录进行筛选
+      _openid: _.exists(true)
+    })
+    .orderBy('createDate', 'desc')
+    .get()
+    .then(res => {
+      // 获取集合数据，或获取根据查询条件筛选后的集合数据。
+      console.log('[云数据库] [答题记录] 查询成功')
+      console.log(res.data)
+      let data = res.data || [];
+      let historyList = [];
+      data.forEach(item => {
+        item.date = this.formatTime(item.createDate)
+        historyList.push(item)
+      })
+
+      // 更新数据到页面展示
+      this.setData({
+        historyList
+      });
+
+      // 隐藏 loading 提示框
+      wx.hideLoading();
+    })
+  },
+
+  formatTime(date) {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    return [year, month, day].map(this.formatNumber).join('/') + ' ' + [hour, minute].map(this.formatNumber).join(':')
+  },
+  
+  formatNumber(n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  }
+})
---
+++ pages/question/result.js
@@ -0,0 +1,62 @@
+/*// pages/results/results.js
+const app = getApp();
+const db = wx.cloud.database();
+const activityRecord = db.collection('activityRecord');
+*/
+Page({
+  data: {
+    totalScore: null,
+    wrong: 0,
+    zql: null,
+  },
+
+  onLoad(options) {
+    // 查看答题成绩
+    this.getExamResult(options.id);
+  },
+
+  // 系统自动判分
+  getExamResult(id){
+    wx.showLoading({
+      title: '系统判分中'
+    });
+    activityRecord
+    .doc(id)
+    .get()
+    .then(res => {
+      let examResult = res.data;
+      
+      let { wrong, totalScore } = examResult;
+      this.setData({
+        totalScore,
+        wrong,
+        zql: (20-wrong)/20*100
+      })
+
+      wx.hideLoading();
+    })
+  },
+
+  // 再答一次
+  toDoTestAgain(){
+    wx.reLaunch({
+      url: '../question/test'
+    })
+  },
+
+  // 返回首页
+  toIndex(){
+    wx.reLaunch({
+      url: '../question/question'
+    })
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+    return {
+      title: '快来参加福大计算机学院院庆答题！'
+    }
+  }
+})
